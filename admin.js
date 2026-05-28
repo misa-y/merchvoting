2f2d9ecdc30b497f1a019d2e6c56e8008b6ad596
misa-y <mishayamamoto0@gmail.com>
2026-05-28 18:56:03 +0900

    Update admin.js

--- admin.js
+++ admin.js
@@ -2,7 +2,18 @@ const STORAGE_KEY = "merch-mockup-voter:v1";
 const DB_NAME = "merch-mockup-voter";
 const DB_STORE = "state";
 const DB_VERSION = 1;
-let DESIGN_CATALOG = [];
+const DEFAULT_DESIGN_CATALOG = [
+  { id: "charcoal_foldover_pants", name: "Charcoal Foldover Pants", image: "./mockups/charcoal_foldover_pants.png" },
+  { id: "polkadot_white_tank", name: "Polkadot White Tank", image: "./mockups/polkadot_white_tank.png" },
+  { id: "sequin_tank", name: "Sequin Tank", image: "./mockups/sequin_tank.png" },
+  { id: "mustangbobiscalling_shirt", name: "Mustang Bob Is Calling Shirt", image: "./mockups/mustangbobiscalling_shirt.png" },
+  { id: "asij_patchwork_tank", name: "ASIJ Patchwork Tank", image: "./mockups/asij_patchwork_tank.png" },
+  { id: "senior_hoodie", name: "Senior Hoodie", image: "./mockups/senior_hoodie.png" },
+  { id: "senior_pants", name: "Senior Pants", image: "./mockups/senior_pants.png" },
+  { id: "pov_shirt", name: "POV Shirt", image: "./mockups/pov_shirt.png" },
+  { id: "AP_exam_survivor_shirt", name: "AP Exam Survivor Shirt", image: "./mockups/AP_exam_survivor_shirt.png" },
+  { id: "athleisure_longsleeve", name: "Athleisure Longsleeve", image: "./mockups/athleisure_longsleeve.png" },
+];
 
 let db = null;
 let state = {
@@ -37,23 +48,6 @@ function dbGet(key) {
 
     request.onsuccess = () => resolve(request.result);
     request.onerror = () => reject(request.error);
-  });
-}
-
-function loadCatalog() {
-  return new Promise((resolve) => {
-    const script = document.createElement("script");
-    script.src = `./catalog.js?v=${Date.now()}`;
-    script.onload = () => {
-      DESIGN_CATALOG = Array.isArray(window.DESIGN_CATALOG) ? window.DESIGN_CATALOG : [];
-      resolve();
-    };
-    script.onerror = () => {
-      DESIGN_CATALOG = Array.isArray(window.DESIGN_CATALOG) ? window.DESIGN_CATALOG : [];
-      console.error("Could not load catalog.js");
-      resolve();
-    };
-    document.head.append(script);
   });
 }
 
