2db7a481157eff00ab007eac38d34797603c0855
misa-y <mishayamamoto0@gmail.com>
2026-05-28 19:04:54 +0900

    Add files via upload

---
+++ catalog.js
@@ -0,0 +1,12 @@
+window.DESIGN_CATALOG = [
+  { id: "charcoal_foldover_pants", name: "Charcoal Foldover Pants", image: "./mockups/charcoal_foldover_pants.png" },
+  { id: "polkadot_white_tank", name: "Polkadot White Tank", image: "./mockups/polkadot_white_tank.png" },
+  { id: "sequin_tank", name: "Sequin Tank", image: "./mockups/sequin_tank.png" },
+  { id: "mustangbobiscalling_shirt", name: "Mustang Bob Is Calling Shirt", image: "./mockups/mustangbobiscalling_shirt.png" },
+  { id: "asij_patchwork_tank", name: "ASIJ Patchwork Tank", image: "./mockups/asij_patchwork_tank.png" },
+  { id: "senior_hoodie", name: "Senior Hoodie", image: "./mockups/senior_hoodie.png" },
+  { id: "senior_pants", name: "Senior Pants", image: "./mockups/senior_pants.png" },
+  { id: "pov_shirt", name: "POV Shirt", image: "./mockups/pov_shirt.png" },
+  { id: "AP_exam_survivor_shirt", name: "AP Exam Survivor Shirt", image: "./mockups/AP_exam_survivor_shirt.png" },
+  { id: "athleisure_longsleeve", name: "Athleisure Longsleeve", image: "./mockups/athleisure_longsleeve.png" },
+];
