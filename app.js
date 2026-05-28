b12f2dc9e98e25e3487cb674c85fd3b517383550
misa-y <mishayamamoto0@gmail.com>
2026-05-28 19:10:04 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ const DB_NAME = "merch-mockup-voter";
 const DB_STORE = "state";
 const DB_VERSION = 1;
 const DEFAULT_DESIGN_CATALOG = [
-  { id: "charcoal_foldover_pants", name: "Charcoal Foldover Pants", image: "./mockups/charcoal_foldover_pants.png" },
-  { id: "polkadot_white_tank", name: "Polkadot White Tank", image: "./mockups/polkadot_white_tank.png" },
-  { id: "sequin_tank", name: "Sequin Tank", image: "./mockups/sequin_tank.png" },
-  { id: "mustangbobiscalling_shirt", name: "Mustang Bob Is Calling Shirt", image: "./mockups/mustangbobiscalling_shirt.png" },
-  { id: "asij_patchwork_tank", name: "ASIJ Patchwork Tank", image: "./mockups/asij_patchwork_tank.png" },
-  { id: "senior_hoodie", name: "Senior Hoodie", image: "./mockups/senior_hoodie.png" },
-  { id: "senior_pants", name: "Senior Pants", image: "./mockups/senior_pants.png" },
-  { id: "pov_shirt", name: "POV Shirt", image: "./mockups/pov_shirt.png" },
-  { id: "AP_exam_survivor_shirt", name: "AP Exam Survivor Shirt", image: "./mockups/AP_exam_survivor_shirt.png" },
-  { id: "athleisure_longsleeve", name: "Athleisure Longsleeve", image: "./mockups/athleisure_longsleeve.png" },
+  { id: "charcoal_foldover_pants", name: "Charcoal Foldover Pants", image: "./charcoal_foldover_pants.png" },
+  { id: "polkadot_white_tank", name: "Polkadot White Tank", image: "./polkadot_white_tank.png" },
+  { id: "sequin_tank", name: "Sequin Tank", image: "./sequin_tank.png" },
+  { id: "mustangbobiscalling_shirt", name: "Mustang Bob Is Calling Shirt", image: "./mustangbobiscalling_shirt.png" },
+  { id: "asij_patchwork_tank", name: "ASIJ Patchwork Tank", image: "./asij_patchwork_tank.png" },
+  { id: "senior_hoodie", name: "Senior Hoodie", image: "./senior_hoodie.png" },
+  { id: "senior_pants", name: "Senior Pants", image: "./senior_pants.png" },
+  { id: "pov_shirt", name: "POV Shirt", image: "./pov_shirt.png" },
+  { id: "AP_exam_survivor_shirt", name: "AP Exam Survivor Shirt", image: "./AP_exam_survivor_shirt.png" },
+  { id: "athleisure_longsleeve", name: "Athleisure Longsleeve", image: "./athleisure_longsleeve.png" },
 ];
 let DESIGN_CATALOG = [];
 
